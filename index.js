ec63fec66d5b4586557b7c3d787ca3fb27145041
Anca Puscasu <ancapuscas@ymail.com>
2021-11-10 18:49:11 -0500

    Added validation logic for registering users and updating user info
    Added validation for Username, Password and Email

--- index.js
+++ index.js
@@ -41,6 +41,10 @@ app.use(cors({
 })); 
 
 
+//Import <express-validator> - Middleware for validating methods on the backend
+const { check, validationResult } = require('express-validator');
+
+
 //Import <passport> module, <passport.js> and <auth.js> files
 const passport = require('passport');
 require('./passport');
@@ -144,34 +148,49 @@ app.get('/users/:Username', passport.authenticate("jwt", { session: false }), (r
         Birthdate: Date
     }
 */
-app.post('/users', (req, res) => {
-    let hashedPassword = Users.hashPassword(req.body.Password);
-    Users.findOne({ Username: req.body.Username}) // Search to see if a user with the requested username already exists.
-        .then ((user) => {
-            if (user) {
-                return res.status(400).send(req.body.Username + " already exists"); //If user is found, send a response that " USER " already exists.
-            } else {
-                Users
-                    .create({
-                        FirstName: req.body.FirstName,
-                        LastName: req.body.LastName,
-                        Username: req.body.Username,
-                        Email: req.body.Email,
-                        Password: hashedPassword,
-                        Birthdate: req.body.Birthdate
-                    })
-                    .then((user) => {
-                        res.status(201).json(user) })
-                    .catch((err) => {
-                        console.error(err);
-                        res.status(500).send('Error: ' + err);
-                    });
-                }
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send('Error: ' + err);
-        });
+app.post('/users', 
+    [
+        check ('Username', 'Username can only contain letter and numbers - no special characters allowed').isAlphanumeric(),
+        check ('Username', 'Username must be 5 characters long').isLength({min: 5}),
+        check ('Password', 'Password is required').not().isEmpty(),
+        check ('Password', 'Password must be at least 8 characters long').isLength({min: 8}),
+        check ('Email', 'Valid email is required').isEmail()
+    ],(req, res) => {
+
+    //check the validation object for errors
+        let errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array () });
+        }
+
+        let hashedPassword = Users.hashPassword(req.body.Password); 
+        Users.findOne({ Username: req.body.Username}) // Search to see if a user with the requested username already exists.
+            .then ((user) => {
+                if (user) {
+                    return res.status(400).send(req.body.Username + " already exists"); //If user is found, send a response that " USER " already exists.
+                } else {
+                    Users
+                        .create({
+                            FirstName: req.body.FirstName,
+                            LastName: req.body.LastName,
+                            Username: req.body.Username,
+                            Email: req.body.Email,
+                            Password: hashedPassword,
+                            Birthdate: req.body.Birthdate
+                        })
+                        .then((user) => {
+                            res.status(201).json(user) })
+                        .catch((err) => {
+                            console.error(err);
+                            res.status(500).send('Error: ' + err);
+                        });
+                    }
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            });
         
 });
 
@@ -188,26 +207,41 @@ app.post('/users', (req, res) => {
         Birthdate: Date
     }
 */
-app.put('/users/:Username', passport.authenticate("jwt", { session: false }), (req, res) => {
-    Users.findOneAndUpdate({ Username: req.params.Username},
-        {$set: {
-            FirstName: req.body.FirstName,
-            LastName: req.body.LastName,
-            Username: req.body.Username,
-            Email: req.body.Email,
-            Password: req.body.Password,
-            Birthdate: req.body.Birthdate
+app.put('/users/:Username', 
+    [
+        check ('Username', 'Username can only contain letter and numbers - no special characters allowed').isAlphanumeric(),
+        check ('Username', 'Username must be 5 characters long').isLength({min: 5}),
+        check ('Password', 'Password is required').not().isEmpty(),
+        check ('Password', 'Password must be at least 8 characters long').isLength({min: 8}),
+        check ('Email', 'Valid email is required').isEmail()
+    ], passport.authenticate("jwt", { session: false }), (req, res) => {
+
+        let errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
         }
-    },
-    { new: true },
-    (err, updatedUser) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Error: ' + err);
-        } else {
-            res.json(updatedUser);
-        }
-    });
+
+        let hashedPassword = Users.hashPassword(req.body.Password);
+
+        Users.findOneAndUpdate({ Username: req.params.Username},
+            {$set: {
+                FirstName: req.body.FirstName,
+                LastName: req.body.LastName,
+                Username: req.body.Username,
+                Email: req.body.Email,
+                Password: hashedPassword,
+                Birthdate: req.body.Birthdate
+            }
+        },
+        { new: true },
+        (err, updatedUser) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updatedUser);
+            }
+        });
 });
 
 
