e3131c7607d8c66e2de79dd13ccb805186e12671
Anca Puscasu <ancapuscasu@ymail.com>
2022-01-20 14:52:59 -0700

    added allowed origins to cors

--- index.js
+++ index.js
@@ -31,7 +31,18 @@ app.use(express.static('public')); // middleware for serving static files
 
 //Import <cors> - Middleware for controlling which domains have access
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://ancas-myflix-app.netlify.app'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 
 //Import <express-validator> - Middleware for validating methods on the backend
